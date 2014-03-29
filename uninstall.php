47ab6796233d61627ecd86eb5f5935407d3332fc
Corey Martin <coreym@gmail.com>
2014-03-29 16:57:33 -0400

    update copyright

--- uninstall.php
+++ uninstall.php
@@ -6,12 +6,10 @@
  * @author    Corey Martin <coreym@gmail.com>
  * @license   GPL-2.0+
  * @link      http://wordpress.org/plugins
- * @copyright 2013 Corey Martin
+ * @copyright Plugin (c) Corey Martin, TwentyTwenty (c) ZURB
  */
 
 // If uninstall not called from WordPress, then exit
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
-
-// TODO: Define uninstall functionality here
